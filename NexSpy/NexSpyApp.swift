5b1c931b986aa46b5fb60fff4c6bace1ff65c207
hrakhshani <hojjat.rakhshani@gmail.com>
2026-04-04 10:53:40 +0200

    Initial Commit

---
+++ NexSpy/NexSpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NexSpyApp.swift
+//  NexSpy
+//
+//  Created by Hojjat Rakhshani on 04/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct NexSpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
